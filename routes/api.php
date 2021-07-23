077fa5f0363ccc714dfefc297c3bbb76e92d7818
Nathan James <nathanjms@outlook.com>
2021-07-23 11:25:22 +0100

    Basic Model Usage

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('products', function (Request $request) {
+    return Product::all();
+});
+
+Route::post('/products', function() {
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is Product One.',
+        'price' => '99.99'
+    ]);
 });
