05160264d3f06f8e7684cf2aee481a73928c01b7
Nathan James <nathanjms@outlook.com>
2021-07-23 12:01:23 +0100

    Sort routes ready for authentication

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Route::resource('products', ProductController::class);
+
+// Public Routes
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('products', [ProductController::class, 'store']);
+    Route::put('products/{id}', [ProductController::class, 'update']);
+    Route::delete('products', [ProductController::class, 'delete']);
+}); 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductController::class);
-Route::get('products/search/{name}', [ProductController::class, 'search']);
 
 
-// Route::get('products', [ProductController::class, 'index']);
-// Route::post('products', [ProductController::class, 'store']);
